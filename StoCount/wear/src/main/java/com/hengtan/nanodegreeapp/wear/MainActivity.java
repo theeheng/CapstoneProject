110bec6163bb67f1ec58dbcd48f4ba73bcbcf3ef
theeheng <theeheng@yahoo.com>
2016-01-19 17:47:59 +0000

    fix: add code for wearable list view

--- StoCount/wear/src/main/java/com/hengtan/nanodegreeapp/wear/MainActivity.java
+++ StoCount/wear/src/main/java/com/hengtan/nanodegreeapp/wear/MainActivity.java
@@ -1,13 +1,22 @@
 package com.hengtan.nanodegreeapp.wear;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.support.wearable.view.WearableListView;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity implements WearableListView.ClickListener  {
 
     private TextView mTextView;
+
+    private DemoItemAdapter mAdapter;
+    private List<DemoItem> mData = new ArrayList<DemoItem>();
+    private WearableListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,50 @@ public class MainActivity extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
+        setupData();
+        setupListView();
+    }
+
+    @Override
+    public void onClick(WearableListView.ViewHolder viewHolder) {
+        DemoItem item = mData.get(viewHolder.getPosition());
+        startActivity(item.getIntent());
+    }
+
+    @Override
+    public void onTopEmptyRegionClick() {
+        // For now, do nothing
+    }
+
+    public void setupData() {
+       // mData.add(new DemoItem(getString(R.string.demo_item_bad_layout), new Intent(this, BadLayoutActivity.class)));
+
+    }
+
+    public void setupListView() {
+        mListView = (WearableListView) findViewById(R.id.wearable_list);
+        mAdapter = new DemoItemAdapter(mData);
+        mListView.setAdapter(mAdapter);
+
+        // Note that this is NOT setting an OnClickListener, but a ClickListener
+        mListView.setClickListener(this);
+    }
+
+    public static class DemoItem {
+        private String mName;
+        private Intent mIntent;
+
+        public DemoItem(String name, Intent intent) {
+            mName = name;
+            mIntent = intent;
+        }
+
+        public String getName() {
+            return mName;
+        }
+
+        public Intent getIntent() {
+            return mIntent;
+        }
     }
 }
